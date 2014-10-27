8da0d23b40bcc077568fb2efead3597e1aa83c2a
An <oming.kr@gmail.com>
2014-10-28 01:04:56 +0900

    초기 네트워크 설정 완료

---
+++ src/TcpServer.java
@@ -0,0 +1,73 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 클라이언트의 연결을 기다리는 서버 쓰레드를 생성합니다.
+ * 
+ * @author An
+ *
+ */
+public class TcpServer implements Runnable {
+	ServerSocket serverSocket;
+	Thread[] threadArr;
+
+//	public static void main(String[] args) {
+//		// 5개의 쓰레드를 생성하는 서버를 생성한다.
+//		TcpServer server = new TcpServer(5);
+//		server.start();
+//	}
+
+	public TcpServer(int num) {
+		try {
+			// 서버 소켓을 생성하여 7777번 포트와 바인딩.
+			serverSocket = new ServerSocket(7777);
+			System.out.println(getTime() + " 서버가 준비되었습니다.");
+
+			threadArr = new Thread[num];
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void start() {
+		for (int i = 0; i < threadArr.length; i++) {
+			threadArr[i] = new Thread(this);
+			threadArr[i].start();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				System.out.println(getTime() + " 가 연결 요청을 기다립니다.");
+
+				Socket socket = serverSocket.accept();
+				System.out.println(getTime() + " " + socket.getInetAddress()
+						+ "로부터 연결요청이 들어왔습니다.");
+
+				OutputStream out = socket.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(out);
+
+				dos.writeUTF("[Notice] Test Message1 from Server");
+				System.out.println(getTime() + " 데이터를 전송하였습니다.");
+
+				dos.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	static String getTime() {
+		String name = Thread.currentThread().getName();
+		SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
+		return f.format(new Date()) + name;
+	}
+}
